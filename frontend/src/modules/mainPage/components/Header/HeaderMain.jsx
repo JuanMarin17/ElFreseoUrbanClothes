c09a384ac22521631ee8c0269c8c82316547868c
JuanG <juandavidgome336@gmail.com>
2026-02-23 09:27:38 -0500

    new

---
+++ frontend/src/modules/mainPage/components/Header/HeaderMain.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function HeaderMain() {
+  return (
+    <header>
+        <div className='logo'>
+            El Freseo
+        </div>
+        
+       <div className="navegation">
+         <ul>
+            <li></li>
+        </ul>
+       </div>
+
+       <div className="icon">
+            
+       </div>
+
+    </header>
+  )
+}
